a7da774155308413327d5399fc81749799cefa45
Daler <108170856+01-Tolibovich@users.noreply.github.com>
2023-01-13 17:05:09 +0500

    first commit

---
+++ your/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      Струнами сердца. Кто он, первый таджикистанец, участник One Beat?
+    </h1>
+    <h2>
+      Струнами сердца. Кто он, первый таджикистанец, участник One Beat?
+    </h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic sit sequi, sapiente, earum illo deleniti dolorem nihil fugit corporis sint modi soluta. Corporis fugiat in reiciendis rem facere ad?
+    Ipsum, eaque earum. Officia aspernatur reiciendis et laborum animi eveniet unde enim perferendis, dolores sit error vero voluptate repellat? Facere quas molestias non architecto modi iure autem ratione maiores assumenda.
+    Ut aliquid magnam in tempore illo omnis quasi eaque earum maxime autem rem consectetur deserunt provident labore quisquam fuga ab fugit placeat facilis ullam veniam, nostrum iure? Iure, quidem est.</p>
+    <span>Lorem, ipsum dolor.</span>
+    <a href="#">Lorem, ipsum.</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ your/pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-bold underline bg-red-700 text-slate-100">
+    Hello world!
+  </h1>
+</template>
